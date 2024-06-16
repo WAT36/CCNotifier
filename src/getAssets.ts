0cfe3ba5320fe30a7a5f288e20167b0be5745618
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-06-16 20:19:53 +0900

    getAssets関数化

--- src/getAssets.ts
+++ src/getAssets.ts
@@ -3,29 +3,41 @@ import crypto from "crypto";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const apiKey = process.env.API_KEY || "";
-const secretKey = process.env.API_SECRET_KEY || "";
+export const getAssets = async () => {
+  const apiKey = process.env.API_KEY || "";
+  const secretKey = process.env.API_SECRET_KEY || "";
 
-const timestamp = Date.now().toString();
-const method = "GET";
-const endPoint = process.env.API_ENDPONT || "";
-const path = "/v1/account/assets";
+  const timestamp = Date.now().toString();
+  const method = "GET";
+  const endPoint = process.env.API_ENDPONT || "";
+  const path = "/v1/account/assets";
 
-const text = timestamp + method + path;
-const sign = crypto.createHmac("sha256", secretKey).update(text).digest("hex");
-const options = {
-  headers: {
-    "API-KEY": apiKey,
-    "API-TIMESTAMP": timestamp,
-    "API-SIGN": sign,
-  },
-};
+  const text = timestamp + method + path;
+  const sign = crypto
+    .createHmac("sha256", secretKey)
+    .update(text)
+    .digest("hex");
+  const options = {
+    headers: {
+      "API-KEY": apiKey,
+      "API-TIMESTAMP": timestamp,
+      "API-SIGN": sign,
+    },
+  };
 
-axios
-  .get(endPoint + path, options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
+  return new Promise<any>((resolve, reject) => {
+    axios
+      .get(endPoint + path, options)
+      .then(function (response) {
+        console.log(response.data);
+        // response.dataに実際のデータが入っている
+        let result: string = response.data;
+        resolve(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+        // エラー時に実行
+        reject(error);
+      });
   });
+};
