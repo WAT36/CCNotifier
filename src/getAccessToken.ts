dcb39f452fb08bdac76126bd7ef428ef20f33e4f
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-06-16 20:30:21 +0900

    間違えた

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -40,5 +40,3 @@ export const getAccessToken = async () => {
       });
   });
 };
-
-getAccessToken();
--- src/getAssets.ts
+++ src/getAssets.ts
@@ -41,3 +41,5 @@ export const getAssets = async () => {
       });
   });
 };
+
+getAssets();
