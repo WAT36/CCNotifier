5305ea23f7575b7a33c5dfa99e2d04e35140763e
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-11-13 23:42:01 +0900

    販売所レート取得・DB更新するバッチ作成

---
+++ src/updateBrandBidAsk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 引数の数が２つかチェックする
+if [ "$#" -ne 2 ]; then
+  echo "Error: Usage $0 brand bid/ask"
+  exit 1
+fi
+
+BRAND=$1
+BIDASK=$2
+
+PWD=`pwd`
+DIR=`dirname $0`
+cd $DIR
+
+# pythonスクリプト実行し値取得
+echo "--- getShopRate start ---"
+rate=$(python3 python/getShopRate.py ${BRAND} ${BIDASK})
+echo "--- getShopRate end   ---"
+if [ $rate = '' ]; then
+  echo "Error: Python Failed. Try again!"
+  cd $PWD
+  exit 1
+fi
+
+# tsバッチ実行し値をDBに登録
+echo "--- updateBrandBidAsk start ---"
+npx ts-node ts/updateBrandBidAsk.ts ${BRAND} ${BIDASK} ${rate}
+echo "--- updateBrandBidAsk end   ---"
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo "Error occured in ts."
+  cd $PWD
+  exit 1
+fi
+
+cd $PWD
+exit 0
