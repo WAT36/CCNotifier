55b3857ad7b97f614a4c4889fdbe286e92e07d1a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-11-15 23:50:08 +0900

    一個一個とるのではなくまとめて関連づけてとるようにした

--- src/ts/isSellTime.ts
+++ src/ts/isSellTime.ts
@@ -20,51 +20,57 @@ const brand = process.argv[2].toUpperCase();
 
 (async function isSellTime() {
   try {
+    // 銘柄のデータ取得
+    const brandData = await prisma.brand.findUnique({
+      select: {
+        name: true,
+        now_yen_bet: {
+          select: {
+            yen_bet: true,
+          },
+        },
+        now_amount: {
+          select: {
+            contract_amount: true,
+            givetake_amount: true,
+            now_amount: true,
+          },
+        },
+        latest_shop_trade: {
+          select: {
+            buysell_category: true,
+            contract_rate: true,
+          },
+        },
+        brandBidAsk: {
+          select: {
+            bid_price: true,
+            ask_price: true,
+          },
+        },
+      },
+      where: {
+        name: brand,
+      },
+    });
+
+    if (!brandData) {
+      console.error("銘柄のデータがありません");
+      return;
+    }
+
     // 現在掛けている円
-    const yenBet = (
-      await prisma.now_yen_bet.findUnique({
-        where: {
-          brand,
-        },
-      })
-    )?.yen_bet;
-
+    const yenBet = brandData.now_yen_bet?.yen_bet;
     // 現在の保有数量
-    const nowAmount = (
-      await prisma.now_amount.findUnique({
-        where: {
-          brand,
-        },
-      })
-    )?.now_amount;
-
+    const nowAmount = brandData.now_amount?.now_amount;
     // 最後に買った時のレート
-    const lastBuyRate = (
-      await prisma.latest_shop_trade.findFirst({
-        where: {
-          brand,
-          buysell_category: "買",
-        },
-      })
-    )?.contract_rate;
-
+    const lastBuyRate =
+      brandData.latest_shop_trade?.buysell_category === "買" &&
+      brandData.latest_shop_trade.contract_rate;
     // 今の売却レート
-    const nowSellRate = (
-      await prisma.brandBidAsk.findUnique({
-        where: {
-          brand,
-        },
-      })
-    )?.ask_price;
-
+    const nowSellRate = brandData.brandBidAsk?.ask_price;
     // 今の売却レート
-    const nowBuyRate = (
-      await prisma.brandBidAsk.findUnique({
-        where: {
-          brand,
-        },
-      })
-    )?.bid_price;
+    const nowBuyRate = brandData.brandBidAsk?.bid_price;
 
     if (!nowAmount || nowAmount === new Decimal(0)) {
       console.log("保有数量0です");
