f0795a554bfe0d99dae206c60e94fbf39839f58b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-11-27 23:05:33 +0900

    ECRレポジトリ作成

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
@@ -12,5 +13,10 @@ export class InfraStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // ECR repository
+    new ecr.Repository(this, "CCNotifierRepository", {
+      repositoryName: "ccnotifier",
+    });
   }
 }
