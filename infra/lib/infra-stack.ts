8127c1c55baa4b36c24af33503bf89fdc50720ac
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-02 13:56:40 +0900

    Batch コンピューティング環境、ジョブキューまで

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as batch from "aws-cdk-lib/aws-batch";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,7 +14,7 @@ export class InfraStack extends cdk.Stack {
     });
 
     // VPC
-    new ec2.Vpc(this, "CCNotifierVPC", {
+    const vpc = new ec2.Vpc(this, "CCNotifierVPC", {
       ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
       subnetConfiguration: [
         {
@@ -23,6 +24,42 @@ export class InfraStack extends cdk.Stack {
         },
       ],
       vpcName: "CCNotifierVPC",
+    });
+
+    // securityGroup
+    const batchSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "BatchSecurityGroup",
+      { vpc }
+    );
+
+    // Batch
+    // Compute Environment
+    const batchComputeEnvironment = new batch.CfnComputeEnvironment(
+      this,
+      "CCNotifierBatchComputingEnvironment",
+      {
+        type: "MANAGED",
+        computeEnvironmentName: "CCNotifierBatchComputingEnvironment",
+        computeResources: {
+          type: "FARGATE",
+          maxvCpus: 256,
+          subnets: vpc.publicSubnets.map((subnet) => subnet.subnetId),
+          securityGroupIds: [batchSecurityGroup.securityGroupId],
+        },
+      }
+    );
+
+    // Job queue
+    new batch.CfnJobQueue(this, "CCNotifierBatchJobQueue", {
+      jobQueueName: "CCNotifierBatchJobQueue",
+      priority: 1,
+      computeEnvironmentOrder: [
+        {
+          order: 1,
+          computeEnvironment: batchComputeEnvironment.attrComputeEnvironmentArn,
+        },
+      ],
     });
   }
 }
