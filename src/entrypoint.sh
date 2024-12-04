665ea21014765f1efef59e6acd411d17aa6b6057
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-04 09:34:53 +0900

    allCheckSellTime実行追加

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-pwd
-echo '---'
-ls
-echo '---'
-
 # npx ts-node src/ts/getRate.ts
 # npx ts-node src/ts/updateBrandBidAsk.ts btc bid
 
 # all update
+echo '---'
 if [ -n "$ALLUPDATE" ] ; then
     src/allUpdateBrandBidAsk.sh
 fi
 
 # assets compare
+echo '---'
 npx ts-node src/ts/compareDataAndAssets.ts
+
+# check sell time
+echo '---'
+src/allCheckSellTime.sh
