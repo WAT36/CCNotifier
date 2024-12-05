280699ecd1beefe9fba758a81e416538346fb9d8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-06 00:33:56 +0900

    sh修正

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# npx ts-node src/ts/getRate.ts
-# npx ts-node src/ts/updateBrandBidAsk.ts btc bid
-
 # all update
 echo '---'
 if [ -n "$ALLUPDATE" ] ; then
     src/allUpdateBrandBidAsk.sh
 fi
 
-# assets compare
-echo '---'
-npx ts-node src/ts/compareDataAndAssets.ts
-
-# check sell time
-echo '---'
-npx ts-node src/ts/allCheckSellTime.ts
+npx ts-node src/ts/entrypoint.ts
