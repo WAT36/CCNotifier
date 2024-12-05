17c0599a2f752f214212ab35b641bf0f6441ccf5
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-06 00:13:06 +0900

    外部からの呼び出しに対応

--- src/ts/postWebhook.ts
+++ src/ts/postWebhook.ts
@@ -2,14 +2,6 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
-// 引数チェック
-if (process.argv.length !== 3) {
-  console.error("Error: Usage: npx ts-node src/postWebhook.ts text");
-  process.exit(1);
-}
-
-const text = process.argv[2];
-
 export const postWebhook = async (text: string) => {
   const webHookURL = process.env.WEBHOOK_URL || "";
   return await fetch(webHookURL, {
@@ -20,4 +12,10 @@ export const postWebhook = async (text: string) => {
   });
 };
 
-postWebhook(text);
+// 引数チェック
+if (process.argv[1] === __filename && process.argv.length !== 3) {
+  console.error("Error: Usage: npx ts-node src/postWebhook.ts text");
+  process.exit(1);
+} else if (process.argv[1] === __filename) {
+  postWebhook(process.argv[2]);
+}
