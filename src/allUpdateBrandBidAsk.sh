92ed3a68156b6538ea8a3637e42b076609bb99df
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-06 10:43:31 +0900

    update失敗表示テスト

--- src/allUpdateBrandBidAsk.sh
+++ src/allUpdateBrandBidAsk.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-source src/config.sh
-
 PWD=`pwd`
 DIR=`dirname $0`
 cd $DIR
 
+source ./config.sh
+
+failed=()
 for brand in "${BRANDS[@]}" ; do
     for operation in "${BIDASK[@]}" ; do
         ./updateBrandBidAsk.sh ${brand} ${operation}
+        if [ $? -eq 1 ]; then
+            failed+=("${brand}-${operation}")
+        fi
     done
 done
+
+echo "失敗したもの↓"
+echo ${failed}
 
 cd $PWD
 exit 0
