b9745923cf822cc0b8b2b614131630d7a79681c6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-09 23:02:07 +0900

    app.handler レート取得、更新設定　実験

--- src/python/app.py
+++ src/python/app.py
@@ -1,6 +1,17 @@
 import json
+from getShopRate import get_shop_rate
+
 def handler(event, context):
+    result = {}
+    brands = ['btc']
+    for brand in brands:
+        brand_result = {}
+        for ope in ['bid','ask']:
+            price = get_shop_rate(brand,ope)
+            brand_result[ope] = price
+        result[brand] =  brand_result
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"tokens": "Hello! World"})
+        "body": json.dumps(result)
     }
---
+++ src/python/constant.py
@@ -0,0 +1,1 @@
+BRAND_NAME_LIST = ['btc','eth','bch','ltc','xrp','xlm','bat','xtz','qtum','dot','atom','ada','mkr','dai','link','doge','sol','fil','sand','chz','avax']
