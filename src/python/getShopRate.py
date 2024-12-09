f3dcf47ccc905d2d43a3965b76449083aad8ceea
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-09 23:01:46 +0900

    レート取得　関数化

--- src/python/getShopRate.py
+++ src/python/getShopRate.py
@@ -5,14 +5,13 @@ import time
 
 # htmlの解析とデータフレームへ
 from bs4 import BeautifulSoup
-# import re
-# import pandas as pd
-# import numpy as np
 
 import os
 import os.path
 from dotenv import load_dotenv
 import sys
+
+from constant import BRAND_NAME_LIST
 
 # 文字列が数字かを判定する関数
 def is_num(s):
@@ -25,61 +24,66 @@ def is_num(s):
     else:
         return True
 
-# 引数チェック
-args = sys.argv
-if len(args) != 3:
-    print("Error: Usage:{0} brand bid/ask".format(args[0]))
-    sys.exit(1)
-
-brand = args[1]
-bid_ask = args[2]
-
-brand_name_list = ['btc','eth','bch','ltc','xrp','xlm','bat','xtz','qtum','dot','atom','ada','mkr','dai','link','doge','sol','fil','sand','chz','avax']
-if brand not in brand_name_list:
-    print("Error: brand must be in {0}".format(brand_name_list))
-    sys.exit(1)
-if bid_ask not in ['bid','ask']:
-    print("Error: you must input bid or ask. not {0}".format(bid_ask))
-    sys.exit(1)
 
 
-# .envファイルの内容を読み込見込む
-dotenv_path = os.path.join(os.path.dirname(__file__), '../../.env')
-load_dotenv(dotenv_path)
+def get_shop_rate(brand,bid_ask):
+    if brand not in BRAND_NAME_LIST:
+        print("Error: brand must be in {0}".format(BRAND_NAME_LIST))
+        sys.exit(1)
+    if bid_ask not in ['bid','ask']:
+        print("Error: you must input bid or ask. not {0}".format(bid_ask))
+        sys.exit(1)
 
-# Firefox オプションを設定　ウインドウ非表示モード（--headless）
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Firefox(options=options)
+    # Firefox オプションを設定　ウインドウ非表示モード（--headless）
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Firefox(options=options)
 
-url = os.environ.get('SHOP_URL_PAGE').format(brand)
+    url = os.environ.get('SHOP_URL_PAGE').format(brand)
 
-driver.get(url)
-result = None
-# 何回か実行して取得する（１回では取れないことがあるため）
-for i in range(5):
-    time.sleep(1)
-    # ページソースを取得
-    html = driver.page_source
-    
-    # 解析し取得
-    soup = BeautifulSoup(html, "html.parser")
-    tag_list = soup.find_all("p" if bid_ask == 'bid' else 'td', class_="l-brand__rate__information__text jsc-price-{0}".format(bid_ask))
-    if len(tag_list)==0:
-        # 値を取得できなかった場合は次へ
-        continue
-    else:
-        result = tag_list[0].text.strip().replace(',','')
-        if is_num(result):
-            # 値を取得できたらそこで終了
-            break
-        else:
-            continue
-else:
-    print("")
+    driver.get(url)
     result = None
+    # 何回か実行して取得する（１回では取れないことがあるため）
+    for i in range(5):
+        time.sleep(1)
+        # ページソースを取得
+        html = driver.page_source
+        
+        # 解析し取得
+        soup = BeautifulSoup(html, "html.parser")
+        tag_list = soup.find_all("p" if bid_ask == 'bid' else 'td', class_="l-brand__rate__information__text jsc-price-{0}".format(bid_ask))
+        if len(tag_list)==0:
+            # 値を取得できなかった場合は次へ
+            continue
+        else:
+            result = tag_list[0].text.strip().replace(',','')
+            if is_num(result):
+                # 値を取得できたらそこで終了
+                break
+            else:
+                continue
+    else:
+        print("")
+        result = None
 
-driver.quit()
+    driver.quit()
 
-# 値を返す
-sys.stdout.write(result if result is not None else '')
+    # 値を返す
+    sys.stdout.write(result if result is not None else '')
+    return result
+
+if __name__ == "__main__": 
+    # 引数チェック
+    args = sys.argv
+    if len(args) != 3:
+        print("Error: Usage:{0} brand bid/ask".format(args[0]))
+        sys.exit(1)
+
+    brand = args[1]
+    bid_ask = args[2]
+
+    # .envファイルの内容を読み込見込む
+    dotenv_path = os.path.join(os.path.dirname(__file__), '../../.env')
+    load_dotenv(dotenv_path)
+
+    get_shop_rate(brand,bid_ask)
