3857bbb7543faf5101d6e52f9677595e0ebd4159
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-12-22 16:23:02 +0900

    handler実行確認

--- src/index.ts
+++ src/index.ts
@@ -14,63 +14,63 @@ export const handler = async (event: any, context: any) => {
   console.log("index handler test!!!.");
   console.log("Event:", JSON.stringify(event));
   console.log("Context:", JSON.stringify(context));
-  if (event.source === "aws.s3") {
-    // S3 アップロードイベントの時
-    const s3Event = event.detail;
-    const bucketName = s3Event.bucket.name;
-    const objectKey = decodeURIComponent(
-      s3Event.object.key.replace(/\+/g, " ")
-    );
+  // if (event.source === "aws.s3") {
+  //   // S3 アップロードイベントの時
+  //   const s3Event = event.detail;
+  //   const bucketName = s3Event.bucket.name;
+  //   const objectKey = decodeURIComponent(
+  //     s3Event.object.key.replace(/\+/g, " ")
+  //   );
 
-    // S3からファイルを取得
-    const command = new GetObjectCommand({
-      Bucket: bucketName,
-      Key: objectKey,
-    });
-    const response = await s3.send(command);
+  //   // S3からファイルを取得
+  //   const command = new GetObjectCommand({
+  //     Bucket: bucketName,
+  //     Key: objectKey,
+  //   });
+  //   const response = await s3.send(command);
 
-    if (!response.Body) {
-      throw new Error("File body is empty");
-    }
+  //   if (!response.Body) {
+  //     throw new Error("File body is empty");
+  //   }
 
-    const pipeline = util.promisify(stream.pipeline);
-    const results: any[] = [];
+  //   const pipeline = util.promisify(stream.pipeline);
+  //   const results: any[] = [];
 
-    // CSVファイルの処理
-    await pipeline(
-      response.Body as stream.Readable,
-      csv(),
-      new stream.Writable({
-        objectMode: true,
-        write: (chunk, _, callback) => {
-          results.push(chunk);
-          callback();
-        },
-      })
-    );
+  //   // CSVファイルの処理
+  //   await pipeline(
+  //     response.Body as stream.Readable,
+  //     csv(),
+  //     new stream.Writable({
+  //       objectMode: true,
+  //       write: (chunk, _, callback) => {
+  //         results.push(chunk);
+  //         callback();
+  //       },
+  //     })
+  //   );
 
-    const registeredLine = await registerDataByLambda(results);
-    await postWebhook(`${registeredLine} 個のデータを登録しました。`);
-  } else {
-    // それ以外（定期スケジュール実行）
-    // all update rate
-    await allUpdateShopRate();
+  //   const registeredLine = await registerDataByLambda(results);
+  //   await postWebhook(`${registeredLine} 個のデータを登録しました。`);
+  // } else {
+  //   // それ以外（定期スケジュール実行）
+  //   // all update rate
+  //   await allUpdateShopRate();
 
-    // assets compare
-    const compareResult = await compareDataAndAssets();
+  //   // assets compare
+  //   const compareResult = await compareDataAndAssets();
 
-    // all check sell time
-    const allCheckResult = await allCheckSellTime();
+  //   // all check sell time
+  //   const allCheckResult = await allCheckSellTime();
 
-    // send data
-    const requestData =
-      "--- compare NG ---\n" +
-      (compareResult.ng.length === 0
-        ? "全て相違なし！\n"
-        : compareResult.ng.join("\n")) +
-      "---            ---\n" +
-      "--- sell check ---\n" +
-      allCheckResult.join("\n");
-    await postWebhook(requestData);
-  }
+  //   // send data
+  //   const requestData =
+  //     "--- compare NG ---\n" +
+  //     (compareResult.ng.length === 0
+  //       ? "全て相違なし！\n"
+  //       : compareResult.ng.join("\n")) +
+  //     "---            ---\n" +
+  //     "--- sell check ---\n" +
+  //     allCheckResult.join("\n");
+  //   await postWebhook(requestData);
+  // }
 };
