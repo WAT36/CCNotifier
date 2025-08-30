08a4e00a0ae268ff696f890fd5f612a3cc646acb
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-08-30 11:55:19 +0900

    Next設定変更

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // ← これが超重要（out/ に静的ファイル出力）
+  images: { unoptimized: true }, // next/image を使うなら必須
+  // trailingSlash: true,        // 各ページを /page/index.html にしたいなら
 };
-
-export default nextConfig;
+module.exports = nextConfig;
