e71971c25ba6fc62db3d6d23b0e2e4f25497a15c
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-12-06 17:39:51 +0900

    不具合あったので変更　テーブル定義見直し

--- prisma/migrations/20251206083848_priceratehistory/migration.sql
+++ prisma/migrations/20251206083848_priceratehistory/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
-CREATE TABLE "rateHistory" (
+CREATE TABLE "priceRateHistory" (
+    "id" SERIAL NOT NULL,
     "brand" VARCHAR(4) NOT NULL,
     "bid_price" DECIMAL(65,30),
     "ask_price" DECIMAL(65,30),
     "created_time" TIMESTAMP(6),
 
-    CONSTRAINT "rateHistory_pkey" PRIMARY KEY ("brand")
+    CONSTRAINT "priceRateHistory_pkey" PRIMARY KEY ("id")
 );
