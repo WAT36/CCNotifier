636f8bbf0a1843388f906016061447ef37e4a7b2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-02-12 00:02:55 +0900

    取引所ticker データ取得の型定義

--- src/getTraderTicker.ts
+++ src/getTraderTicker.ts
@@ -3,16 +3,27 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
+export type TradeTickerData = {
+  ask: string,
+  bid: string,
+  high: string,
+  last: string,
+  low: string,
+  symbol: string,
+  timestamp: string,
+  volume: string
+}
+
 export const getTradeTicker = async () => {
   const endPoint = process.env.API_PUBLIC_ENDPONT || '';
   const path = '/v1/ticker';
 
-  return await new Promise<any>((resolve, reject) => {
+  return await new Promise<TradeTickerData[]>((resolve, reject) => {
     axios
       .get(endPoint + path)
       .then(function (response) {
         // response.dataに実際のデータが入っている
-        let result: string = response.data;
+        let result: TradeTickerData[] = response.data;
         console.log(result);
         resolve(result);
       })
